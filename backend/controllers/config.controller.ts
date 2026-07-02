9716af33f6d6a0c089730ea2221326250df70247
Alex Cunningham <admin@k9alk.net>
2026-07-02 15:19:51 -0400

    feat(controllers): work more on config controllers - finish addMenu - finish addItems - prepare for new kafka race-condition fix

--- backend/controllers/config.controller.ts
+++ backend/controllers/config.controller.ts
@@ -9,13 +9,19 @@ import { User } from "../model/users.model";
 
 import type { IItem } from "../model/items.model";
 import type { ICategory } from "../model/category.model";
-import type { IMenu } from "../model/menu.model";
+import { Menu, type IMenu } from "../model/menu.model";
 
 import { Category } from "../model/category.model";
 
 import type { IMenu } from "../model/menu.model"
 
-
+/**
+ * AdminController is responsible for handling administrative actions such as adding items, categories, menus, and users. It extends the BaseController and uses Kafka for event-driven communication.
+ * @extends BaseController
+ * @controller /admin
+ * @kafkaTopic config
+ * 
+ */
 @Controller("/admin")
 export class AdminController extends BaseController {
 
@@ -23,37 +29,51 @@ export class AdminController extends BaseController {
         super({ topic: "config" });
     }
 
+    /**
+     * Adds new items to the system. It expects a JSON payload containing item details and generates a unique SKU if not provided. The method also verifies the user's authorization before proceeding.
+     * @param {Request} req - The HTTP request object containing the item details in JSON format.
+     * @returns {Promise<Response>} - A promise that resolves to an HTTP response indicating the success or failure of the operation.
+     * @AuthLevel 2 - Admin only 
+    **/
     @Post("/items/add", 2)
     async addItems(req: Request): Promise<Response> {
-        const json: { items?: unknown, categories?: unknown, menu?: unknown, sku: string } = await req.json();
-        const items = json.items;
-        const categories = json.categories;
-        const menu = json.menu;
-        const sku = json.sku ?? "MISC-" + Math.floor(Math.random() * 1000)
-
-        // const auth = req.headers.get("Authorization")?.replace("Bearer ", "");
-        // if (!auth) {
-            
-        //     return Response.json({ success: false, message: "Unauthorized No Token" }, { status: 401 });
-        // }
-        // const user = await (UserController as unknown as { verify?: (token: string) => Promise<unknown> }).verify?.(auth);
-        
-        // log.withContext({ user }).trace("User verified for adding items");
-
+        const items = await req.json() as { name: string, description?: string, price?: number, category: Category, sku?: string }[];
+        const entities: Item[] = [];
         try {
-            await this.kafka([{
-                key: "item.add",
-                value: sku
-            }])
-            const entities = new Item({
-                sku,
-                name: json.name,
-                description: json.description,
-                price: json.price
+        for (const item of items) {
+            const { name, description, price, category, sku } = item;
 
-            });
-        
-            await entities.save()
+            const solvedItem = {
+                name: name,
+                description: description ?? '',
+                price: price ?? 0,
+                category: category,
+                sku: sku ?? "MISC-" + Math.floor(Math.random() * 1000)
+            }
+            const newSku = sku ?? "MISC-" + Math.floor(Math.random() * 1000)
+
+                await this.kafka([{
+                    key: "item.add",
+                    value: 
+                    solvedItem.sku +
+                    solvedItem.name + 
+                    solvedItem.description + 
+                    solvedItem.price + 
+                    solvedItem.category?.id
+                }])
+
+
+                const item: Item  = new Item({
+                    sku: solvedItem.sku,
+                    name: solvedItem.name,
+                    description: solvedItem.description,
+                    price: solvedItem.price,
+                    category: solvedItem.category,
+                });
+                entities.push(item);
+                await item.save();
+            }
+
             log.withMetadata({ entities }).trace("Added new item to repository");
             return Response.json({ success: true, data: entities }, { status: 200 });
         } catch (error) {
@@ -62,18 +82,61 @@ export class AdminController extends BaseController {
         }
     }
 
+
+    /**
+     * Adds a new menu to the system. It expects a JSON payload containing menu details including name, items, categories, and active dates. The method verifies the existence of items and categories before creating the menu.
+     * @param req 
+     * @returns 
+     * @AuthLevel 2 - Admin only
+     */
     @Post("/menu/add", 0)
     async addMenu(req: Request): Promise<Response> {
-        const { items, categories } = await req.json() as IMenu[];
+        const {name, items, categories, activeDates } = await req.json() as Menu[];
+
         try {
-            const entities = await User.getAll();
-            return Response.json({ success: true, data: entities }, { status: 200 });
+            let existingItems: Item[] = [];
+            for (const item of items) {
+                //check if item exists (if has id load id, or search by name)
+                let existingItem = (await Item.byId(item.id)) || (await Item.byName(item.name))[0];
+                if (existingItem) {
+                    existingItems.push(existingItem);
+                } else {
+                    throw new Error(`Item not found: ${item.name}`);
+                }
+            }
+
+            let existingCategories: Category[] = [];
+            for (const category of categories) {
+                let existingCategory = (await Category.byId(category.id)) || (await Category.byName(category.name))[0];
+                if (existingCategory) {
+                    existingCategories.push(existingCategory);
+                } else {
+                    throw new Error(`Category not found: ${category.name}`);
+                }
+            }
+
+            let newMenu = new Menu({
+                name: name,
+                items: existingItems,
+                categories: existingCategories,
+                createdAt: new Date(),
+                updatedAt: new Date(),
+            });
+
+            return Response.json({ success: true, data: newMenu }, { status: 200 });
+        
         } catch (error) {
             log.error('Error fetching users: ' + error);
-            return new Response("Internal Server Error", { status: 500 });
+            return new Response("API Error:", { status: 402 });
         }
     }
 
+    /**
+     * Adds new categories to the system. It expects a JSON payload containing category details and saves them to the repository. The method verifies the user's authorization before proceeding.
+     * @param {Request} req - The HTTP request object containing the category details in JSON format.
+     * @returns {Promise<Response>} - A promise that resolves to an HTTP response indicating the success or failure of the operation.
+     * @AuthLevel 2 - Admin only 
+    **/
     @Post("/category/add", 0)
     async addCategory(req: Request): Promise<Response> {
         let categories  = await req.json() as  ICategory[] ;
@@ -108,6 +171,12 @@ export class AdminController extends BaseController {
         }
     }
 
+    /**
+     * Adds a new user to the system. It expects a JSON payload containing the username and password, hashes the password, and saves the new user to the repository. The method verifies the user's authorization before proceeding.
+     * @param {Request} req - The HTTP request object containing the user details in JSON format.
+     * @returns {Promise<Response>} - A promise that resolves to an HTTP response indicating the success or failure of the operation.
+    *  @AuthLevel 2 - Admin only 
+    **/
     @Post("/user/add", 0)
     async addUser(req: Request): Promise<Response> {
         const { username, password }= await req.json() as { username: string, password: string };
