ddf3c626fb072a7ac5beb0b835d66a6a5cba2ec9
Alex Cunningham <admin@k9alk.net>
2026-08-22 21:29:33 -0400

    feat: each action button has a action function. Handle pay button function added

--- web/app/components/side-actions.tsx
+++ web/app/components/side-actions.tsx
@@ -6,16 +6,22 @@ function SideActions() {
   const actions = Array.from({ length: 4 }, (_, index) => ({
     id: index,
     label: `Action ${index + 1}`,
+    action: () => console.log(`Action ${index + 1} clicked`),
   }));
+
+  const handlePayNow = () => {
+    return;
+  }
+
   return (
     <div className={`side-actions grid grid-cols-2 grid-rows-3 gap-2 max-h-100 ${gridStyles.sideActions}`} id='side-actions'>
       {actions.map(action => (
-        <Button key={action.id} className='action-item'>
+        <Button key={action.id} className='action-item' onClick={action.action}>
           {action.label}
         </Button>
       ))}
 
-      <Button key='PayNow' className='col-span-full'>
+      <Button key='PayNow' onClick={handlePayNow} className='col-span-full'>
         Pay Now
       </Button>
     </div>
